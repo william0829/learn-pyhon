185939cdd2324aaceac308b048c0b0c00dfdb394
william0829 <44386696+william0829@users.noreply.github.com>
2018-10-24 15:16:08 +0800

    Create the_dict.py

---
+++ sample/basic/the_dict.py
@@ -0,0 +1,12 @@
+#!/usr/bin/enc python3
+#-*- coding utf-8 -*-
+
+d = {
+	'Micheal':95,
+	'Bob':75,
+	'Tracy':85
+}
+print('d[\'Micheal\'] = ',d['Micheal'])
+print('d[\'Bob\'] = ',d['Bob'])
+print('d[\'Tracy\'] = ',d['Tracy'])
+print('d.get(\'Thomas\',-1) = ',d.get('Thomas',-1))
