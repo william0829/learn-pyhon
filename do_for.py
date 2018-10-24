cd4fb7fb26eef7ee34b05751c0143d5be8fc23e7
william0829 <44386696+william0829@users.noreply.github.com>
2018-10-24 14:15:44 +0800

    Create do_for.py

---
+++ do_for.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+# -*- coding utf-8 -*-
+
+#打印列表L的内容
+L=['a','b','c','d','e']
+for l in L:
+    print(l)
+exit()
+
+#打印从1-10每个数相加
+sum=0
+for x in [1,2,3,4,5,6,7,8,9,10]:
+    sum=sum+x
+    print(sum)
+exit()
