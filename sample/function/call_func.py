5c916baf55eed96d4be22a4b0cf7a9cbb0afb206
william0829 <44386696+william0829@users.noreply.github.com>
2018-10-24 16:00:34 +0800

    Create call_func.py

---
+++ sample/function/call_func.py
@@ -0,0 +1,23 @@
+#!/usr/bin/enc python3
+#-*- coding utf-8 -*-
+
+#函数abs绝对值，max最大值，int取整数
+#float取浮点数，str取字符，bool布尔值，hex转换十六进制
+
+print('abs(100) = ',abs(100))
+print('abs(-10) = ',abs(-10))
+a=abs
+print('a = abs,a(-50) = ',a(-50))
+print('int(123) = ',int(123))
+print('int(12.34) = ',int(12.34))
+print('float(12.34) = ',float(12.34))
+print('str(12.34) = ',str(12.34))
+print('bool(1) = ',bool(1))
+print('bool(10) = ',bool(10))
+print('bool(\'\') = ',bool(''))
+print('bool(\'s\') = ',bool('s'))
+print('hex(255) = ',hex(255))
+print('hex(1000) = ',hex(1000))
+print('max(5,8,10) = ',max(5,8,10))
+print('min(5,8,10) = ',min(5,8,10))
+print('sum([5,8,10]) = ',sum([5,8,10]))
